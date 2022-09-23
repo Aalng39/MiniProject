e32e0d38ef31a0ad91fb27a66a7efc33597c8b31
Aalng39 <Aalng39>
2022-09-23 15:00:25 +0800

    Added Twilio

---
+++ src/main/java/vttp2022/app/miniproject/Service/TwilioService.java
@@ -0,0 +1,23 @@
+package vttp2022.app.miniproject.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class TwilioService {
+    // Find your Account SID and Auth Token at twilio.com/console
+    // and set the environment variables. See http://twil.io/secure
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+19289166595"),
+                new com.twilio.type.PhoneNumber("+6597298598"),
+                "This is the ship that made the Kessel Run in fourteen parsecs?")
+            .create();
+
+        System.out.println(message.getSid());
+    }
+}
